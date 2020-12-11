05f6796e6ccf85344a1d0c45373df725b4615588
Eric Ballard <ericballard7@gmail.com>
2020-12-11 03:50:48 -0500

    + Loading custom model/animations + Particle system

--- js/index.js
+++ js/index.js
@@ -1,8 +1,65 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js";
 
 import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js";
+import { FBXLoader } from "https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/loaders/FBXLoader.js";
 
-import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/loaders/GLTFLoader.js";
+const _VS = `
+uniform float pointMultiplier;
+attribute float size;
+attribute float angle;
+attribute vec4 colour;
+varying vec4 vColour;
+varying vec2 vAngle;
+void main() {
+  vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
+  gl_Position = projectionMatrix * mvPosition;
+  gl_PointSize = size * pointMultiplier / gl_Position.w;
+  vAngle = vec2(cos(angle), sin(angle));
+  vColour = colour;
+}`;
+
+const _FS = `
+uniform sampler2D diffuseTexture;
+varying vec4 vColour;
+varying vec2 vAngle;
+void main() {
+  vec2 coords = (gl_PointCoord - 0.5) * mat2(vAngle.x, vAngle.y, -vAngle.y, vAngle.x) + 0.5;
+  gl_FragColor = texture2D(diffuseTexture, coords) * vColour;
+}`;
+
+class LinearSpline {
+  constructor(lerp) {
+    this._points = [];
+    this._lerp = lerp;
+  }
+
+  AddPoint(t, d) {
+    this._points.push([t, d]);
+  }
+
+  Get(t) {
+    let p1 = 0;
+
+    for (let i = 0; i < this._points.length; i++) {
+      if (this._points[i][0] >= t) {
+        break;
+      }
+      p1 = i;
+    }
+
+    const p2 = Math.min(this._points.length - 1, p1 + 1);
+
+    if (p1 == p2) {
+      return this._points[p1][1];
+    }
+
+    return this._lerp(
+        (t - this._points[p1][0]) / (
+            this._points[p2][0] - this._points[p1][0]),
+        this._points[p1][1], this._points[p2][1]);
+  }
+}
+
 
 class BasicWorldDemo {
   constructor() {
@@ -37,9 +94,9 @@ class BasicWorldDemo {
 
     this._scene = new THREE.Scene();
 
-    let light = new THREE.DirectionalLight(0xffffff, 5.0);
-    light.position.set(20, 100, 10);
-    light.target.position.set(0, 0, 0);
+    let light = new THREE.DirectionalLight(0xffffff, 5);
+    light.position.set(0, 900, 45);
+    light.target.position.set(0, 900, 0);
     light.castShadow = true;
     light.shadow.bias = -0.001;
     light.shadow.mapSize.width = 2048;
@@ -60,17 +117,29 @@ class BasicWorldDemo {
     // User-Controls
     const controls = new OrbitControls(this._camera, this._threejs.domElement);
     controls.minDistance = 25;
-    controls.maxDistance = 150;
+    controls.maxDistance = 250;
     controls.update();
 
     // Sky box
     let materialArray = [];
-    let texture_ft = new THREE.TextureLoader().load("/resources/uw_ft.jpg");
-    let texture_bk = new THREE.TextureLoader().load("/resources/uw_bk.jpg");
-    let texture_up = new THREE.TextureLoader().load("/resources/uw_up.jpg");
-    let texture_dn = new THREE.TextureLoader().load("/resources/uw_dn.jpg");
-    let texture_rt = new THREE.TextureLoader().load("/resources/uw_rt.jpg");
-    let texture_lf = new THREE.TextureLoader().load("/resources/uw_lf.jpg");
+    let texture_ft = new THREE.TextureLoader().load(
+      "/resources/skybox/uw_ft.jpg"
+    );
+    let texture_bk = new THREE.TextureLoader().load(
+      "/resources/skybox/uw_bk.jpg"
+    );
+    let texture_up = new THREE.TextureLoader().load(
+      "/resources/skybox/uw_up.jpg"
+    );
+    let texture_dn = new THREE.TextureLoader().load(
+      "/resources/skybox/uw_dn.jpg"
+    );
+    let texture_rt = new THREE.TextureLoader().load(
+      "/resources/skybox/uw_rt.jpg"
+    );
+    let texture_lf = new THREE.TextureLoader().load(
+      "/resources/skybox/uw_lf.jpg"
+    );
 
     materialArray.push(new THREE.MeshBasicMaterial({ map: texture_ft }));
     materialArray.push(new THREE.MeshBasicMaterial({ map: texture_bk }));
@@ -92,24 +161,39 @@ class BasicWorldDemo {
       })
     );
 
-    this.box.position.set(0, 10, 0);
-    this.box.castShadow = true;
-    this.box.receiveShadow = true;
-    this._scene.add(this.box);
-
     this.fps = document.getElementById("fps");
 
-    const loader = new GLTFLoader();
-    loader.load("/resources/fish/fish.gltf", function (model) {
-      const fish = model.scene.children[0];
-      fish.position.set(0, 30, -30);
+    this._mixers = [];
+    this._particles = [];
+    this._previousRAF = null;
 
-      fish.receiveShadow = true;
-      fish.castShadow = true;
-      _APP._scene.add(fish);
+    // Init particle system
+    this._particles = new ParticleSystem({
+      parent: this._scene,
+      camera: this._camera,
     });
 
+    this._LoadAnimatedModel();
     this._RAF();
+  }
+
+  _LoadAnimatedModel() {
+    const loader = new FBXLoader();
+    loader.setPath("/resources/");
+
+    loader.load("salmon.fbx", (model) => {
+      model.scale.setScalar(0.1);
+      //this.mixer = new THREE.AnimationMixer(model);
+      //this._mixers.push(this.mixer);
+
+      // const anim = model.animations[0];
+      //const idle = _APP.mixer.clipAction(anim);
+
+      // idle.play();
+
+      this.man = model;
+      this._scene.add(this.man);
+    });
   }
 
   _OnWindowResize() {
@@ -123,24 +207,236 @@ class BasicWorldDemo {
 
     if (secondTracker == null) secondTracker = now;
 
-    requestAnimationFrame(() => {
+    requestAnimationFrame((frame) => {
+      if (this._previousRAF === null) this._previousRAF = frame;
+
       const newSecond = now - secondTracker > 1000;
 
       if (newSecond) {
         this.fps.innerText = "FPS: " + framesRendered;
         secondTracker = now;
         framesRendered = 1;
-        // this.box.applyMatrix4(new THREE.Matrix4().makeTranslation(0, 0.25, 0));
+
+        this.man.applyMatrix4(new THREE.Matrix4().makeTranslation(-1, 0, 0));
       } else {
-        // Cap at 60 fps
-        if (framesRendered <= 60) {
-          framesRendered += 1;
-          this._threejs.render(this._scene, this._camera);
-        }
+        framesRendered += 1;
+        this._threejs.render(this._scene, this._camera);
       }
 
       this._RAF();
+      this._Step(frame - this._previousRAF);
+      this._previousRAF = frame;
     });
+  }
+
+  _Step(timeElapsed) {
+    const timeElapsedS = timeElapsed * 0.001;
+
+    if (this._mixers) this._mixers.map((m) => m.update(timeElapsedS));
+
+    if (this._particles) this._particles.Step(timeElapsedS);
+  }
+}
+
+class ParticleSystem {
+  constructor(params) {
+    const uniforms = {
+      diffuseTexture: {
+        value: new THREE.TextureLoader().load("./resources/bubble.png"),
+      },
+      pointMultiplier: {
+        value:
+          window.innerHeight / (2.0 * Math.tan((0.5 * 60.0 * Math.PI) / 180.0)),
+      },
+    };
+
+    this._material = new THREE.ShaderMaterial({
+      uniforms: uniforms,
+      vertexShader: _VS,
+      fragmentShader: _FS,
+      blending: THREE.AdditiveBlending,
+      depthTest: true,
+      depthWrite: false,
+      transparent: true,
+      vertexColors: true,
+    });
+
+    this._camera = params.camera;
+    this._particles = [];
+
+    this._geometry = new THREE.BufferGeometry();
+    this._geometry.setAttribute(
+      "position",
+      new THREE.Float32BufferAttribute([], 3)
+    );
+    this._geometry.setAttribute(
+      "size",
+      new THREE.Float32BufferAttribute([], 1)
+    );
+    this._geometry.setAttribute(
+      "colour",
+      new THREE.Float32BufferAttribute([], 4)
+    );
+    this._geometry.setAttribute(
+      "angle",
+      new THREE.Float32BufferAttribute([], 1)
+    );
+
+    this._points = new THREE.Points(this._geometry, this._material);
+
+    params.parent.add(this._points);
+
+    this._alphaSpline = new LinearSpline((t, a, b) => {
+      return a + t * (b - a);
+    });
+    this._alphaSpline.AddPoint(0.0, 0.0);
+    this._alphaSpline.AddPoint(0.1, 1.0);
+    this._alphaSpline.AddPoint(0.6, 1.0);
+    this._alphaSpline.AddPoint(1.0, 0.0);
+
+    this._colourSpline = new LinearSpline((t, a, b) => {
+      const c = a.clone();
+      return c.lerp(b, t);
+    });
+    this._colourSpline.AddPoint(0.0, new THREE.Color(0xffff80));
+    this._colourSpline.AddPoint(1.0, new THREE.Color(0xff8080));
+
+    this._sizeSpline = new LinearSpline((t, a, b) => {
+      return a + t * (b - a);
+    });
+    this._sizeSpline.AddPoint(0.0, 1.0);
+    this._sizeSpline.AddPoint(0.5, 5.0);
+    this._sizeSpline.AddPoint(1.0, 1.0);
+
+    document.addEventListener("keyup", (e) => this._onKeyUp(e), false);
+
+    this._UpdateGeometry();
+  }
+
+  _onKeyUp(event) {
+    switch (event.keyCode) {
+      case 32: // SPACE
+        this._AddParticles();
+        break;
+    }
+  }
+
+  _AddParticles(timeElapsed) {
+    if (!this.gdfsghk) {
+      this.gdfsghk = 0.0;
+    }
+    this.gdfsghk += timeElapsed;
+    const n = Math.floor(this.gdfsghk * 75.0);
+    this.gdfsghk -= n / 75.0;
+
+    for (let i = 0; i < n; i++) {
+      const life = (Math.random() * 0.75 + 0.25) * 10.0;
+      this._particles.push({
+        position: new THREE.Vector3(
+          (Math.random() * 2 - 1) * 1.0,
+          (Math.random() * 2 - 1) * 1.0,
+          (Math.random() * 2 - 1) * 1.0
+        ),
+        size: (Math.random() * 0.5 + 0.5) * 4.0,
+        colour: new THREE.Color(),
+        alpha: 1.0,
+        life: life,
+        maxLife: life,
+        rotation: Math.random() * 2.0 * Math.PI,
+        velocity: new THREE.Vector3(0, -15, 0),
+      });
+    }
+  }
+
+  _UpdateGeometry() {
+    const positions = [];
+    const sizes = [];
+    const colours = [];
+    const angles = [];
+
+    for (let p of this._particles) {
+      positions.push(p.position.x, p.position.y, p.position.z);
+      colours.push(p.colour.r, p.colour.g, p.colour.b, p.alpha);
+      sizes.push(p.currentSize);
+      angles.push(p.rotation);
+    }
+
+    this._geometry.setAttribute(
+      "position",
+      new THREE.Float32BufferAttribute(positions, 3)
+    );
+    this._geometry.setAttribute(
+      "size",
+      new THREE.Float32BufferAttribute(sizes, 1)
+    );
+    this._geometry.setAttribute(
+      "colour",
+      new THREE.Float32BufferAttribute(colours, 4)
+    );
+    this._geometry.setAttribute(
+      "angle",
+      new THREE.Float32BufferAttribute(angles, 1)
+    );
+
+    this._geometry.attributes.position.needsUpdate = true;
+    this._geometry.attributes.size.needsUpdate = true;
+    this._geometry.attributes.colour.needsUpdate = true;
+    this._geometry.attributes.angle.needsUpdate = true;
+  }
+
+  _UpdateParticles(timeElapsed) {
+    for (let p of this._particles) {
+      p.life -= timeElapsed;
+    }
+
+    this._particles = this._particles.filter((p) => {
+      return p.life > 0.0;
+    });
+
+    for (let p of this._particles) {
+      const t = 1.0 - p.life / p.maxLife;
+
+      p.rotation += timeElapsed * 0.5;
+      p.alpha = this._alphaSpline.Get(t);
+      p.currentSize = p.size * this._sizeSpline.Get(t);
+      p.colour.copy(this._colourSpline.Get(t));
+
+      p.position.add(p.velocity.clone().multiplyScalar(timeElapsed));
+
+      const drag = p.velocity.clone();
+      drag.multiplyScalar(timeElapsed * 0.1);
+      drag.x =
+        Math.sign(p.velocity.x) *
+        Math.min(Math.abs(drag.x), Math.abs(p.velocity.x));
+      drag.y =
+        Math.sign(p.velocity.y) *
+        Math.min(Math.abs(drag.y), Math.abs(p.velocity.y));
+      drag.z =
+        Math.sign(p.velocity.z) *
+        Math.min(Math.abs(drag.z), Math.abs(p.velocity.z));
+      p.velocity.sub(drag);
+    }
+
+    this._particles.sort((a, b) => {
+      const d1 = this._camera.position.distanceTo(a.position);
+      const d2 = this._camera.position.distanceTo(b.position);
+
+      if (d1 > d2) {
+        return -1;
+      }
+
+      if (d1 < d2) {
+        return 1;
+      }
+
+      return 0;
+    });
+  }
+
+  Step(timeElapsed) {
+    this._AddParticles(timeElapsed);
+    this._UpdateParticles(timeElapsed);
+    this._UpdateGeometry();
   }
 }
 
